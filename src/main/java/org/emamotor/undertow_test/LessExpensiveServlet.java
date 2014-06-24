9ff21606695ce29fe930ee468bb16639c1c4d9ab
emag <lanabe.lanabe@gmail.com>
2014-06-24 20:27:23 +0900

    Add Less Expensive test

---
+++ src/main/java/org/emamotor/undertow_test/LessExpensiveServlet.java
@@ -0,0 +1,34 @@
+package org.emamotor.undertow_test;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author tanabe
+ */
+@WebServlet("/less-expensive")
+public class LessExpensiveServlet extends HttpServlet {
+
+  private static final int NUM = 5_000_000;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    response.setContentType("text/html; charset=UTF-8");
+    expensiveOperation(NUM);
+    response.getWriter().println("This is Expensive Servlet: NUM: " + NUM + " sec.");
+  }
+
+  private void expensiveOperation(int delay) {
+    List<String> list = new ArrayList<>();
+    for (int i = 0; i < NUM; i++) {
+      Collections.shuffle(list);
+    }
+  }
+
+}
